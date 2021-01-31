e037ce044a06e296263e1db586fb897882c70c49
mehmet halis <mehmethaliscicek@gmail.com>
2021-01-31 23:25:49 +0300

    header section edited

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import {connect} from 'react-redux';
 import {fetchFaults} from './actions/FaultAction';
+import {Drawer, Button, Row, Col} from 'antd';
+import WaterLogo from './drop.svg';
 
-function App(props) {
+
+const App = (props) => {
     const {fetchFaults} = props;
+    const [visible, setvisible] = useState(false);
+    const [faut, setFault] = useState({name: 'halis', surname: 'çiçek'});
+    const [isLoading, setLoading] = useState(false);
+
     useEffect(() => {
         fetchFaults()
     }, [fetchFaults])
 
+    const faults = props.state.FaultReducer.faults
+
+
+    const onClose = () => {
+        setvisible(false)
+    };
+
+    const refresh = () => {
+        setLoading(true)
+        fetchFaults()
+        setLoading(false)
+    }
     return (
         <div className="App">
-            <h1>Hello World!</h1>
-            <button onClick={fetchFaults}>
-                Hi
-            </button>
+            <img src={WaterLogo} alt={'Su Logosu'} className={'waterLogo'}/>
+            <Button loading={isLoading} shape="round" className={'refreshButton'} onClick={refresh}>Yenile</Button>
+            <h2>İzmir Büyükşehir Belediyesi Güncel Arıza Kaynaklı Su Kesintileri</h2>
+
+            <hr/>
+            <Row align={'middle'} justify={'center'}>
+                {
+                    faults.map((fault) => {
+                        return (
+                            <Col xs={2} sm={4} md={8} lg={8} xl={8} key={fault.ArizaID}>
+                                <Button
+                                    onClick={() => {
+                                        setFault({name: fault.IlceAdi, surname: fault.KesintiTarihi})
+                                        setvisible(true)
+                                    }}> {fault.IlceAdi}</Button>
+                            </Col>)
+                    })
+                }
+
+            </Row>
+
+
+            <Drawer
+                title="Basic Drawer"
+                placement={'bottom'}
+                closable={false}
+                onClose={onClose}
+                visible={visible}
+                key={'bottom'}
+            >
+                <p>{faut.name}</p>
+                <p>{faut.surname}</p>
+            </Drawer>
         </div>
     );
 }
