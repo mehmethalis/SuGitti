578abc39da9578a4fedf927b69e0ef64ffe4b94b
mehmet halis <mehmethaliscicek@gmail.com>
2021-02-01 21:27:16 +0300

    Fault detail drawer created.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ import {Drawer, Button, Row, Col, Spin} from 'antd';
 import WaterLogo from './drop.svg';
 import ResponseResult from "./components/ResponseResult";
 import FaultCard from "./components/FaultCard";
+import sampleData from "./sampleData";
 
 
 const App = (props) => {
     const {fetchFaults} = props;
     const [visible, setVisible] = useState(false);
-    const [faut, setFault] = useState({name: 'halis', surname: 'çiçek'});
+    const [fault, setFault] = useState(sampleData);
 
+    const onClose = () => {
+        setVisible(false)
+    };
 
     useEffect(() => {
         fetchFaults()
     }, [fetchFaults])
 
-    const faults = props.state.FaultReducer.faults
-
-
-    const onClose = () => {
-        setVisible(false)
-    };
+    const faults = props.state.FaultReducer.faults;
 
     const refresh = () => {
         fetchFaults()
@@ -32,41 +31,79 @@ const App = (props) => {
         <div className="App">
             <img src={WaterLogo} alt={'Su Logosu'} className={'waterLogo'}/>
             <Button loading={props.state.FaultReducer.fetching} shape="round" className={'refreshButton'}
-                    onClick={refresh}>Yenile</Button>
+                    onClick={refresh}>Yenile
+            </Button>
             <h2>İzmir Büyükşehir Belediyesi Güncel Arıza Kaynaklı Su Kesintileri</h2>
-
             <hr/>
+
             <Row align={'middle'} justify={'center'} className={'row'}>
                 {props.state.FaultReducer.fetching && <Spin className={'spinner'} size="large"/>}
-                {props.state.FaultReducer.error ? <ResponseResult error={props.state.FaultReducer.error}/> :
-                    faults.map((fault) => {
-                        return (
-                            <Col xs={24} sm={24} md={12} lg={8} xl={8} key={fault.ArizaID}>
-                                <a href={'/#'} onClick={() => {
-                                    setFault({name: fault.IlceAdi, surname: fault.KesintiTarihi})
-                                    setVisible(true)
-                                }}>
-                                    <FaultCard fault={fault}/>
-                                </a>
-
-                            </Col>)
-                    })}
-
-
+                {
+                    props.state.FaultReducer.error ? <ResponseResult error={props.state.FaultReducer.error}/> :
+                        faults.map((fault) => {
+                            return (
+                                <Col xs={24} sm={24} md={12} lg={8} xl={8} key={fault.ArizaID}>
+                                    <a href={'/#'} onClick={() => {
+                                        setFault(fault)
+                                        setVisible(true)
+                                    }}>
+                                        <FaultCard fault={fault}/>
+                                    </a>
+                                </Col>)
+                        })
+                }
             </Row>
-
-
             <Drawer
-                title="Basic Drawer"
+                height={400}
+                bodyStyle={{
+                    backgroundColor: '#aee1e1',
+                    fontWeight: 600,
+                    color: '#0f1123'
+                }}
+                headerStyle={{
+                    backgroundColor: '#aee1e1',
+                    fontWeight: 800,
+                    color: '#0f1123'
+                }}
+                title={<h2 className={'drawerTitle'}>{fault.IlceAdi}</h2>}
                 placement={'bottom'}
-                closable={false}
+                closable={true}
                 onClose={onClose}
                 visible={visible}
                 key={'bottom'}
             >
-                <p>{faut.name}</p>
-                <p>{faut.surname}</p>
+                <Row align={'top'} justify={'center'}>
+                    <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <div className={'drawerSection'}>
+                            <span>Kesinti Tarihi :</span>
+                            <h3>{fault.KesintiTarihi}</h3>
+                            <span>Arıza Giderilme Tarihi:</span>
+                            <h3>{fault.ArizaGiderilmeTarihi}</h3>
+                            <span>Arıza Tipi :</span>
+                            <h3>{fault.Tip}</h3>
+                            <span>Birim :</span>
+                            <h3>{fault.Birim}</h3>
+                        </div>
+                    </Col>
+                    <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <div className={'drawerSection'}>
+                            <span>İlçe :</span>
+                            <h3>{fault.IlceAdi}</h3>
+                            <span>Mahalleler :</span>
+                            <h3>{fault.Mahalleler}</h3>
+                        </div>
+                    </Col>
+                    <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                        <div className={'drawerSection'}>
+                            <span>Açıklama :</span>
+                            <h3>{fault.Aciklama}</h3>
+                            <span>Kesinti Süresi :</span>
+                            <h3>{fault.KesintiSuresi}</h3>
+                        </div>
+                    </Col>
+                </Row>
             </Drawer>
+
         </div>
     );
 }
---
+++ src/sampleData.js
@@ -0,0 +1,20 @@
+const sampleData = () => ({
+    Aciklama: "2208 SOKAKDA YAŞANAN ANA BORU ARIZASI SEBEBİ İLE SOKAK VE GENELİNDE SU KESİNTİSİ YAPILMIŞTIR.",
+    AdSoyad: "SEMİH APAYDIN",
+    ArizaDurumu: "1",
+    ArizaGiderilmeTarihi: "2021-02-01T16:45:00",
+    ArizaID: 130052,
+    ArizaTipID: 1,
+    Birim: "ÇEŞME ŞUBE MÜDÜRLÜĞÜ ÇEŞME TEKNİK AMİRLİĞİ",
+    GuncellemeTarihi: "2021-02-01T11:57:19",
+    IlceAdi: "ÇEŞME",
+    IlceID: 16,
+    KayitTarihi: "2021-02-01T11:56:15",
+    KesintiSuresi: "01.02.2021 saat 11:45  ile 16:45 arasında  yaklaşık 5 saat  su kesintisi olacaktır.",
+    KesintiTarihi: "2021-02-01T11:45:00",
+    MahalleID: [3136],
+    Mahalleler: "FAHRETTİNPAŞA",
+    Ongoru: "2",
+    Tip: "Ana Boru Arızası"
+})
+export default sampleData;
