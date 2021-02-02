527cdbc7182a5c7a3fdfbc35eaf656e4b0898a46
mehmet halis <mehmethaliscicek@gmail.com>
2021-02-02 12:03:39 +0300

    footer created and make responsive

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import WaterLogo from './drop.svg';
 import ResponseResult from "./components/ResponseResult";
 import FaultCard from "./components/FaultCard";
 import sampleData from "./sampleData";
+import {GithubOutlined} from '@ant-design/icons';
 
 
 const App = (props) => {
@@ -28,83 +29,88 @@ const App = (props) => {
         fetchFaults()
     }
     return (
-        <div className="App">
-            <img src={WaterLogo} alt={'Su Logosu'} className={'waterLogo'}/>
-            <Button loading={props.state.FaultReducer.fetching} shape="round" className={'refreshButton'}
-                    onClick={refresh}>Yenile
-            </Button>
-            <h2>İzmir Büyükşehir Belediyesi Güncel Arıza Kaynaklı Su Kesintileri</h2>
-            <hr/>
+        <>
+            <div className="App">
+                <img src={WaterLogo} alt={'Su Logosu'} className={'waterLogo'}/>
+                <Button loading={props.state.FaultReducer.fetching} shape="round" className={'refreshButton'}
+                        onClick={refresh}>Yenile
+                </Button>
+                <h2>İzmir Büyükşehir Belediyesi Güncel Arıza Kaynaklı Su Kesintileri</h2>
+                <hr/>
 
-            <Row align={'middle'} justify={'center'} className={'row'}>
-                {props.state.FaultReducer.fetching && <Spin className={'spinner'} size="large"/>}
-                {
-                    props.state.FaultReducer.error ? <ResponseResult error={props.state.FaultReducer.error}/> :
-                        faults.map((fault) => {
-                            return (
-                                <Col xs={24} sm={24} md={12} lg={8} xl={8} key={fault.ArizaID}>
-                                    <a href={'/#'} onClick={() => {
-                                        setFault(fault)
-                                        setVisible(true)
-                                    }}>
-                                        <FaultCard fault={fault}/>
-                                    </a>
-                                </Col>)
-                        })
-                }
-            </Row>
-            <Drawer
-                height={400}
-                bodyStyle={{
-                    backgroundColor: '#aee1e1',
-                    fontWeight: 600,
-                    color: '#0f1123'
-                }}
-                headerStyle={{
-                    backgroundColor: '#aee1e1',
-                    fontWeight: 800,
-                    color: '#0f1123'
-                }}
-                title={<h2 className={'drawerTitle'}>{fault.IlceAdi}</h2>}
-                placement={'bottom'}
-                closable={true}
-                onClose={onClose}
-                visible={visible}
-                key={'bottom'}
-            >
-                <Row align={'top'} justify={'center'}>
-                    <Col xs={24} sm={24} md={12} lg={8} xl={8}>
-                        <div className={'drawerSection'}>
-                            <span>Kesinti Tarihi :</span>
-                            <h3>{fault.KesintiTarihi}</h3>
-                            <span>Arıza Giderilme Tarihi:</span>
-                            <h3>{fault.ArizaGiderilmeTarihi}</h3>
-                            <span>Arıza Tipi :</span>
-                            <h3>{fault.Tip}</h3>
-                            <span>Birim :</span>
-                            <h3>{fault.Birim}</h3>
-                        </div>
-                    </Col>
-                    <Col xs={24} sm={24} md={12} lg={8} xl={8}>
-                        <div className={'drawerSection'}>
-                            <span>İlçe :</span>
-                            <h3>{fault.IlceAdi}</h3>
-                            <span>Mahalleler :</span>
-                            <h3>{fault.Mahalleler}</h3>
-                        </div>
-                    </Col>
-                    <Col xs={24} sm={24} md={12} lg={8} xl={8}>
-                        <div className={'drawerSection'}>
-                            <span>Açıklama :</span>
-                            <h3>{fault.Aciklama}</h3>
-                            <span>Kesinti Süresi :</span>
-                            <h3>{fault.KesintiSuresi}</h3>
-                        </div>
-                    </Col>
+                <Row align={'middle'} justify={'center'} className={'row'}>
+                    {props.state.FaultReducer.fetching && <Spin className={'spinner'} size="large"/>}
+                    {
+                        props.state.FaultReducer.error ? <ResponseResult error={props.state.FaultReducer.error}/> :
+                            faults.map((fault) => {
+                                return (
+                                    <Col xs={24} sm={24} md={12} lg={8} xl={8} key={fault.ArizaID}>
+                                        <a href={'/#'} onClick={() => {
+                                            setFault(fault)
+                                            setVisible(true)
+                                        }}>
+                                            <FaultCard fault={fault}/>
+                                        </a>
+                                    </Col>)
+                            })
+                    }
                 </Row>
-            </Drawer>
-
-        </div>
+                <Drawer
+                    height={400}
+                    bodyStyle={{
+                        backgroundColor: '#aee1e1',
+                        fontWeight: 600,
+                        color: '#0f1123'
+                    }}
+                    headerStyle={{
+                        backgroundColor: '#aee1e1',
+                        fontWeight: 800,
+                        color: '#0f1123'
+                    }}
+                    title={<h2 className={'drawerTitle'}>{fault.IlceAdi}</h2>}
+                    placement={'bottom'}
+                    closable={true}
+                    onClose={onClose}
+                    visible={visible}
+                    key={'bottom'}
+                >
+                    <Row align={'top'} justify={'center'}>
+                        <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                            <div className={'drawerSection'}>
+                                <span>Kesinti Tarihi :</span>
+                                <h3>{fault.KesintiTarihi}</h3>
+                                <span>Arıza Giderilme Tarihi:</span>
+                                <h3>{fault.ArizaGiderilmeTarihi}</h3>
+                                <span>Arıza Tipi :</span>
+                                <h3>{fault.Tip}</h3>
+                                <span>Birim :</span>
+                                <h3>{fault.Birim}</h3>
+                            </div>
+                        </Col>
+                        <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                            <div className={'drawerSection'}>
+                                <span>İlçe :</span>
+                                <h3>{fault.IlceAdi}</h3>
+                                <span>Mahalleler :</span>
+                                <h3>{fault.Mahalleler}</h3>
+                            </div>
+                        </Col>
+                        <Col xs={24} sm={24} md={12} lg={8} xl={8}>
+                            <div className={'drawerSection'}>
+                                <span>Açıklama :</span>
+                                <h3>{fault.Aciklama}</h3>
+                                <span>Kesinti Süresi :</span>
+                                <h3>{fault.KesintiSuresi}</h3>
+                            </div>
+                        </Col>
+                    </Row>
+                </Drawer>
+            </div>
+            <footer className={'footer'}>
+                <span className={'copyright'}>©{new Date().getFullYear()} Tüm Hakları Saklıdır. | Veriler <a href={'https://acikveri.bizizmir.com/'} target={'_blank'} rel="noreferrer">Açık Veri İzmir</a> Portalına Aittir. </span>
+                <a className={'github'} href={'https://github.com/mehmethalis/SuGitti'} target={'_blank'}  rel="noreferrer"><GithubOutlined className={'githubIcon'}/> GitHub</a>
+            </footer>
+        </>
     );
 }
 
